b8db2c9fe1be749f5c9d9e5cf5322be7197ec85a
cszyrowska <cecyliaszyrowska@gmail.com>
2026-02-03 12:11:36 +0000

    pages not working on github so changed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Use '/' if you have a custom domain (softocean.net)
+  // Use '/react_softocean/' if you are viewing it at softoceanltd.github.io/react_softocean/
   base: '/', 
 })
