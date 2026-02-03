eeb2575e42bb9f42fedb439aa822e0a1532a2800
cszyrowska <cecyliaszyrowska@gmail.com>
2026-02-03 12:26:01 +0000

    Update base path for github.io URL

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Use '/' if you have a custom domain (softocean.net)
-  // Use '/react_softocean/' if you are viewing it at softoceanltd.github.io/react_softocean/
-  base: '/', 
+  // This tells Vite your site is in a sub-folder named 'react_softocean'
+  base: '/react_softocean/', 
 })
