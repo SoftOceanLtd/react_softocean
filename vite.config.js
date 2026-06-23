95e41b2b6fe416dda1724505a0f12655aba293c1
cszyrowska <cecyliaszyrowska@gmail.com>
2026-06-23 11:51:29 +0100

    Prepare website for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // This tells Vite your site is in a sub-folder named 'react_softocean'
-  base: '/react_softocean/', 
+  base: '/',
 })
